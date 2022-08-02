bcd2f1200717c87a55959c7af3a2f8410fc2377d
Stanisław Pawlaczyk <ctahncnab@gmail.com>
2022-08-02 16:21:15 +0200

    heroku another try

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,15 @@ app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
+const NODE_ENV = process.env.NODE_ENV;
+let dbURI = '';
+
+if(NODE_ENV === 'production') dbURI = 'mongodb+srv://johndoe:admin@cluster0.hjmht.mongodb.net/EcobagStore?retryWrites=true&w=majority';
+else if(NODE_ENV === 'test') dbURI = 'mongodb://localhost:27017/EcobagStore';
+else dbURI = 'mongodb://localhost:27017/EcobagStore';
+
 /* MONGOOSE */
-mongoose.connect('mongodb://localhost:27017/EcobagStore', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.once('open', () => {
   console.log('Successfully connected to the database');
